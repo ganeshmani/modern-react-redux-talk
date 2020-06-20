f265e214f874e0caad8953086c87de4554ba59d3
mac <ganeshmani009@gmail.com>
2020-06-20 09:30:16 +0530

    Updates

---
+++ precache-manifest.d74d951ef1f4463237864ed326992d63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aeb06bbb17b76e7891b138ea4d55608a",
+    "url": "/modern-react-redux-talk/index.html"
+  },
+  {
+    "revision": "474752c9a6fc9b4a64ac",
+    "url": "/modern-react-redux-talk/static/css/main.c838c056.chunk.css"
+  },
+  {
+    "revision": "61f7df7be9e7c1d1b54c",
+    "url": "/modern-react-redux-talk/static/js/2.d4304210.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/modern-react-redux-talk/static/js/2.d4304210.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474752c9a6fc9b4a64ac",
+    "url": "/modern-react-redux-talk/static/js/main.284e6c6b.chunk.js"
+  },
+  {
+    "revision": "61d1fee583b55084b057",
+    "url": "/modern-react-redux-talk/static/js/runtime-main.b95634fa.js"
+  }
+]);
